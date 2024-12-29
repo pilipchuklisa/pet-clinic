715a78d55ecdda0ab753e334e1fa877807e27263
Lisa <pilipchchlisa@gmail.com>
2024-12-29 19:58:17 +0300

    refactoring to common crud interface. closes #11

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/CrudService.java
@@ -0,0 +1,16 @@
+package com.example.petclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    T findById(ID id);
+
+    Set<T> findAll();
+
+    void save(T t);
+
+    void delete(T t);
+
+    void deleteById(ID id);
+}
