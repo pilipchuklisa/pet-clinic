f2e07e1e9ad3b37cc747b612719f96d5d435c719
Lisa <pilipchchlisa@gmail.com>
2025-01-05 13:36:52 +0300

    enhanced owners with pets and contact info. closes #35

--- pet-clinic-data/src/main/java/com/example/petclinic/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/map/OwnerServiceMap.java
@@ -2,10 +2,45 @@ package com.example.petclinic.map;
 
 import com.example.petclinic.model.Owner;
 import com.example.petclinic.services.OwnerService;
+import com.example.petclinic.services.PetService;
+import com.example.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        if (owner != null) {
+            if (owner.getPets() != null) {
+                owner.getPets().forEach(pet -> {
+                    if (pet.getType() != null) {
+                        if (pet.getType().getId() == null) {
+                            petTypeService.save(pet.getType());
+                        }
+                    } else {
+                        throw new RuntimeException("PetType is required");
+                    }
+
+                    pet.setOwner(owner);
+                    if (pet.getId() == null) {
+                        petService.save(pet);
+                    }
+                });
+            }
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
+        return super.save(owner);
+    }
 
     @Override
     public Owner findByLastName(String lastName) {
--- pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -1,24 +1,32 @@
 package com.example.petclinic.bootstrap;
 
 import com.example.petclinic.model.Owner;
+import com.example.petclinic.model.Pet;
 import com.example.petclinic.model.PetType;
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.services.OwnerService;
+import com.example.petclinic.services.PetService;
 import com.example.petclinic.services.PetTypeService;
 import com.example.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetService petService;
     private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetService petService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petService = petService;
         this.petTypeService = petTypeService;
     }
 
@@ -31,17 +39,35 @@ public class DataLoader implements CommandLineRunner {
 
         PetType petType2 = new PetType();
         petType2.setName("pet type 2");
-        petTypeService.save(petType2);
+
+        Pet pet1 = new Pet();
+        pet1.setName("pet 1");
+        pet1.setBirthDate(LocalDate.of(2020, 12, 12));
+        pet1.setType(petType1);
+        pet1 = petService.save(pet1);
+
+        Pet pet2 = new Pet();
+        pet2.setName("pet 2");
+        pet2.setBirthDate(LocalDate.of(2021, 12, 12));
+        pet2.setType(petType2);
 
         Owner owner1 = new Owner();
+        owner1.setAddress("address for owner 1");
+        owner1.setCity("city for owner 1");
+        owner1.setTelephone("12345678");
         owner1.setFirstName("Eva");
         owner1.setLastName("Stone");
+        owner1.setPets(new HashSet<>(List.of(pet1)));
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setId(2L);
+        owner2.setAddress("address for owner 2");
+        owner2.setCity("city for owner 2");
+        owner2.setTelephone("23456789");
         owner2.setFirstName("Elena");
         owner2.setLastName("Quartz");
+        owner2.setPets(new HashSet<>(List.of(pet2)));
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
