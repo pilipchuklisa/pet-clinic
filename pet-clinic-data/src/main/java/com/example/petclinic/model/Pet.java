17523de78684ba3c0fcccf8b06c019a4fe10bed2
Lisa <pilipchchlisa@gmail.com>
2025-01-07 13:18:50 +0300

    added visit entity. closes #40

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
@@ -3,6 +3,7 @@ package com.example.petclinic.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +22,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits;
 
     public String getName() {
         return name;
@@ -52,5 +56,13 @@ public class Pet extends BaseEntity {
 
     public void setOwner(Owner owner) {
         this.owner = owner;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
