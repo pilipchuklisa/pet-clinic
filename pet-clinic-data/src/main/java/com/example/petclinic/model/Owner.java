4d469e9f591f86dcdf431668fef277b997dc459e
Lisa <pilipchchlisa@gmail.com>
2025-01-07 12:56:09 +0300

    converted owners to jpa entity. closes #38

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
@@ -1,12 +1,23 @@
 package com.example.petclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets;
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
@@ -1,12 +1,25 @@
 package com.example.petclinic.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType type;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
