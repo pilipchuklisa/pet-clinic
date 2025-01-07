a8c2730297b7bfc2979a1e18e322509ef4ebf281
Lisa <pilipchchlisa@gmail.com>
2025-01-07 14:47:35 +0300

    added Spring Data JPA Pet Service. closes #45

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,49 @@
+package com.example.petclinic.services.springdatajpa;
+
+import com.example.petclinic.model.Pet;
+import com.example.petclinic.repositories.PetRepository;
+import com.example.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().iterator().forEachRemaining(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
