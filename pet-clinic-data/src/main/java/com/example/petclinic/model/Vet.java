b8352bf1a02cf69b309007fc3bec18b9a8477678
Lisa <pilipchchlisa@gmail.com>
2025-01-07 13:09:44 +0300

    converted vets to jpa entity. closes #39

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Vet.java
@@ -1,9 +1,19 @@
 package com.example.petclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany
+    @JoinTable(
+            name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id")
+    )
     private Set<Specialty> specialties;
 
     public Set<Specialty> getSpecialties() {
