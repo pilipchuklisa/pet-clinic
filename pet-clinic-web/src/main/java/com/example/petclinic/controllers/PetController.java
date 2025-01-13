82ddced49f567a281f529d26a073a128911343b8
Lisa <pilipchchlisa@gmail.com>
2025-01-13 18:24:17 +0300

    added pet attributes, #58

---
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/PetController.java
@@ -0,0 +1,61 @@
+package com.example.petclinic.controllers;
+
+import com.example.petclinic.model.Owner;
+import com.example.petclinic.model.PetType;
+import com.example.petclinic.services.OwnerService;
+import com.example.petclinic.services.PetService;
+import com.example.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@RequestMapping("/owners/{ownerId}/pets")
+@Controller
+public class PetController {
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+//    @ModelAttribute("pet")
+//    public Pet findPet(@PathVariable("ownerId") Long ownerId,
+//                       @PathVariable(name = "petId", required = false) Long petId) {
+//
+//        if (petId == null) {
+//            return new Pet();
+//        }
+//        Owner owner = ownerService.findById(ownerId);
+//
+//        return owner.getPets().stream()
+//                .filter(p -> Objects.equals(p.getId(), petId))
+//                .findFirst()
+//                .get();
+//    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+}
